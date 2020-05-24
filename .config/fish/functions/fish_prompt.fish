9eb55ea738bc21850ac12039f7803940c394c6b6
basbebe <git@bastianbeuttel.com>
2020-05-24 19:11:51 +0200

    change default falllback prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,88 +1,27 @@
-function fish_prompt
-    set -l __last_command_exit_status $status
-
-    if not set -q -g __fish_robbyrussell_functions_defined
-        set -g __fish_robbyrussell_functions_defined
-        function _git_branch_name
-            set -l branch (git symbolic-ref --quiet HEAD 2>/dev/null)
-            if set -q branch[1]
-                echo (string replace -r '^refs/heads/' '' $branch)
-            else
-                echo (git rev-parse --short HEAD 2>/dev/null)
-            end
-        end
-
-        function _is_git_dirty
-            echo (git status -s --ignore-submodules=dirty 2>/dev/null)
-        end
-
-        function _is_git_repo
-            type -q git
-            or return 1
-            git rev-parse --git-dir >/dev/null 2>&1
-        end
-
-        function _hg_branch_name
-            echo (hg branch 2>/dev/null)
-        end
-
-        function _is_hg_dirty
-            echo (hg status -mard 2>/dev/null)
-        end
-
-        function _is_hg_repo
-            fish_print_hg_root >/dev/null
-        end
-
-        function _repo_branch_name
-            _$argv[1]_branch_name
-        end
-
-        function _is_repo_dirty
-            _is_$argv[1]_dirty
-        end
-
-        function _repo_type
-            if _is_hg_repo
-                echo 'hg'
-                return 0
-            else if _is_git_repo
-                echo 'git'
-                return 0
-            end
-            return 1
-        end
-    end
-
-    set -l cyan (set_color -o cyan)
-    set -l yellow (set_color -o yellow)
-    set -l red (set_color -o red)
-    set -l green (set_color -o green)
-    set -l blue (set_color -o blue)
+function fish_prompt --description 'Write out the prompt'
+    set -l last_pipestatus $pipestatus
+    set -l last_status $status
     set -l normal (set_color normal)
 
-    set -l arrow_color "$green"
-    if test $__last_command_exit_status != 0
-        set arrow_color "$red"
-    end
-
-    set -l arrow "$arrow_color➜ "
-    if test "$USER" = 'root'
-        set arrow "$arrow_color# "
-    end
-
-    set -l cwd $cyan(basename (prompt_pwd))
-
-    if set -l repo_type (_repo_type)
-        set -l repo_branch $red(_repo_branch_name $repo_type)
-        set repo_info "$blue $repo_type:($repo_branch$blue)"
-
-        set -l dirty (_is_repo_dirty $repo_type)
-        if test -n "$dirty"
-            set -l dirty "$yellow ✗"
-            set repo_info "$repo_info$dirty"
+    # Color the prompt differently when we're root
+    set -l color_cwd $fish_color_cwd
+    set -l prefix
+    set -l suffix '>'
+    if contains -- $USER root toor
+        if set -q fish_color_cwd_root
+            set color_cwd $fish_color_cwd_root
         end
+        set suffix '#'
     end
 
-    echo -n -s $arrow ' '$cwd $repo_info $normal ' '
+    # If we're running via SSH, change the host color.
+    set -l color_host $fish_color_host
+    if set -q SSH_TTY
+        set color_host $fish_color_host_remote
+    end
+
+    # Write pipestatus
+    set -l prompt_status (__fish_print_pipestatus " [" "]" "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
+
+    echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal $prompt_status $suffix " "
 end
