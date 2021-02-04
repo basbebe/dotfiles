f00801b5fac8ac81052f48975c96f64660d75db6
basbebe <git@bastianbeuttel.com>
2021-02-04 19:42:27 +0100

    edit dotfiles w/ lazygit

--- .config/fish/functions/dotfiles.fish
+++ .config/fish/functions/dotfiles.fish
@@ -1,4 +1,9 @@
 function dotfiles --wraps=git --description='provides git interface to manage dotfiles'
-    git --git-dir=/$HOME/.dotfiles/ --work-tree=/$HOME $argv
+    # invoke lazygit TUI if no arguments are given
+    if test (count $argv) -eq 0
+        lazygit --git-dir=/$HOME/.dotfiles/ --work-tree=/$HOME
+    else
+        git --git-dir=/$HOME/.dotfiles/ --work-tree=/$HOME $argv
+    end
 end
 
