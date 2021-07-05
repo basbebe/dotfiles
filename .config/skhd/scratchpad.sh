29d2e4f6cdbb08d276669b4035b00e700bad3d89
basbebe <git@bastianbeuttel.com>
2021-07-05 14:12:55 +0200

    minor change

--- .config/skhd/scratchpad.sh
+++ .config/skhd/scratchpad.sh
@@ -9,7 +9,7 @@ kitty_cmd="kitty --title $SCRATCHPAD_TITLE \
 scratchpad_id=$(yabai -m query --windows --space | jq --arg title "$SCRATCHPAD_TITLE" -c '.[] | select(.title==$title).id')
 
 if [[ "$scratchpad_id" -lt 1 ]]; then
-  $kitty_cmd $@
+  $kitty_cmd $@ &> /dev/null
 else
   is_minimized=$(yabai -m query --windows --window "$scratchpad_id" | jq '.minimized')
   current_space=$(yabai -m query --spaces --space | jq '.index')
