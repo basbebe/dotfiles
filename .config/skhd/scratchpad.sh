52950ac8185cfdd496bbf105db2e961ac1c0ca09
basbebe <git@bastianbeuttel.com>
2021-10-28 10:14:47 +0200

    rewrite scratchpad

--- .config/skhd/scratchpad.sh
+++ .config/skhd/scratchpad.sh
@@ -1,26 +1,45 @@
 #!/bin/sh
 
-kitty_cmd="kitty --title $SCRATCHPAD_TITLE \
-  --directory $HOME
+kitty_cmd="kitty \
   --config="$HOME/.config/kitty/kitty.conf" \
   --config="$HOME/.config/kitty/themes/gruvbox-$(fish -c 'echo $OS_THEME').conf"
   --override macos_hide_from_tasks=yes \
   --single-instance \
-  --instance-group=scratchpad"
+  --instance-group=scratchpad \
+  --listen-on=unix:/tmp/kitty_listen_scratchpad"
+
+kitty_launch="kitty @ --to=unix:/tmp/kitty_listen_scratchpad \
+  launch \
+  --env SHELL=/usr/local/bin/fish \
+  --title=$SCRATCHPAD_TITLE \
+  --cwd=$HOME \
+  --type=os-window"
 
 scratchpad_id=$(yabai -m query --windows | jq --arg title "$SCRATCHPAD_TITLE" -c '[.[] | select(.title==$title).id'][0])
 
+is_focused()
+{
+  [[ $(yabai -m query --windows --window $1 | jq '.focused') -eq  1 ]]
+}
+is_minimized()
+{
+  [[ $(yabai -m query --windows --window $1 | jq '.minimized') -eq 1 ]]
+}
+
 if [[ "$scratchpad_id" -lt 1 ]]; then
-  SHELL=/usr/local/bin/fish $kitty_cmd "${@}" &> /dev/null ;
+  $kitty_launch "${@}" || \
+  $kitty_cmd $kitty_launch "${@}"
 else
-  is_minimized=$(yabai -m query --windows --window "$scratchpad_id" | jq '.minimized')
   current_space=$(yabai -m query --spaces --space | jq '.index')
-  if [[ "$is_minimized" -eq 1 ]]; then
+  if is_minimized $scratchpad_id ; then
     yabai -m window "$scratchpad_id" --space "$current_space"
-    # yabai -m window "$scratchpad_id" --deminimize
     yabai -m window --focus "$scratchpad_id"
   else
-    yabai -m window "$scratchpad_id" --minimize
+    if is_focused $scratchpad_id ; then
+      yabai -m window "$scratchpad_id" --minimize
+    else
+      yabai -m window --focus "$scratchpad_id"
+    fi
   fi
 fi
 
