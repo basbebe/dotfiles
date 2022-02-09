3def25865f46731a3f447160029f4c795a686779
basbebe <git@bastianbeuttel.com>
2022-02-09 20:11:46 +0100

    remove spacebar

--- .config/spacebar/keyboard-layout.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleSelectedInputSources | command rg -e '"KeyboardLayout Name" = "*([^"]*)"*;' --replace '$1' --only-matching --color never
